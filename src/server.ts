eaa04c7bc6197b9006431e9b51492317734a2740
Marcelo Buongermino <buon.marcelo@hotmail.com>
2022-11-14 22:55:08 -0300

    fix: http port

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import { app } from "./app";
 createConnection();
 
 // inicia o servidor da aplicação em localhost:3333
-app.listen(3333, () => {
+app.listen(3333 || process.env.PORT, () => {
     console.log("Server online on port 3333 ✅");
 });
